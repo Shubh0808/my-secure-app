92a3e8f63a20b389b72728d08280bf25c9ac1a9e
Shubham Kumar <k.shubham@olamnet.com>
2025-07-16 01:23:44 +0530

    Update backend

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const app = express();
 
 app.use(cors());  // Use cors middleware for all routes
 
+app.get('/', (req, res) => {
+  res.send('API is running!');
+});
+
 app.get('/api/health', (req, res) => {
   res.json({ status: 'OK' });
 });
---
+++ sonar-scanner.js
@@ -0,0 +1,12 @@
+const scanner = require('sonarqube-scanner');
+
+scanner({
+  serverUrl: 'http://localhost:9001',
+  token: 'sqp_ba1bc3576017a97a352ef5127b3e4aebac10530b',
+  options: {
+    'sonar.projectKey': 'my-secure-app',
+    'sonar.sources': '.',
+    'sonar.language': 'js',
+    'sonar.sourceEncoding': 'UTF-8',
+  }
+}, () => process.exit());
